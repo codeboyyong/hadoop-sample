55d3904addddff4a0581ed5c7dcac510d48d128d
codeboyyong <codeboyyong@gmail.com>
2014-10-14 22:12:45 -0700

    refine code

--- wordcount_cli/run.sh
+++ wordcount_cli/run.sh
@@ -1,7 +1,9 @@
 mvn clean
 mvn package
+
 #First we copy the things from hadoop jar command
 echo mvn exec:java  -Dexec.mainClass="com.codeboy.hadoop.tools.MRJarRunner" -Dexec.classpathScope="compile"  -Dexec.args="./target/wordcount_cli-1.0.jar  com.codeboy.hadoop.wordcount.WordCount hdfs://localhost:9000/csv/golfnew.csv /tmp/myout"
+
 # now output path owner is "xx"
 mvn exec:java  -Dexec.mainClass="com.codeboy.hadoop.tools.MRJarRunner" -Dexec.classpathScope="compile"  -Dexec.args="./target/wordcount_cli-1.0.jar  com.codeboy.hadoop.wordcount.WordCount hdfs://localhost:9000/csv/golfnew.csv /tmp/myout"
  
